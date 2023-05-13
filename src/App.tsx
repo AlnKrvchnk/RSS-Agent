621cf5d347fd2bfb0355359d9b24264d3a118f8d
Alina <alinakrvchnk@gmail.com>
2023-05-13 16:04:33 +0300

    add MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Grid from '@mui/material/Grid';
+import FilterList from './components/Filter List/FilterList';
+import FilterText from './components/Filter Text/FilterText';
+import { Container, Stack, Typography } from '@mui/material';
+import NewsItem from './components/News Item/NewsItem';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="sm" style={{height:'100vh', paddingTop:'5vh'}}>
+        
+      <Stack spacing={4} position={'relative'}>
+<Stack spacing={2} position={'sticky'} top={0} bgcolor={'white'} zIndex={1} >
+<Typography variant="h2" m={2} align='center' color='primary.dark' fontWeight={'bold'}>RSS-agent</Typography>
+<FilterList/>
+<FilterText/></Stack>
+<Stack spacing={2} padding={'2px'}>
+  <NewsItem></NewsItem>
+  <NewsItem></NewsItem>
+  <NewsItem></NewsItem>
+  <NewsItem></NewsItem>
+</Stack>
+      </Stack></Container>
     </div>
   );
 }
---
+++ src/components/Filter Text/FilterText.tsx
@@ -0,0 +1,6 @@
+import { TextField } from "@mui/material"
+
+const FilterText =()=>{
+   return (<TextField  id="outlined-basic" label="Outlined" variant="outlined" />)
+}
+export default FilterText
